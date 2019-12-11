376d0971f80421d5cc0b08e674c780f987d2b594
Anton Klapatun <klapatun@zao-rim.ru>
2019-12-11 12:21:41 +0700

    fix: Контрольная завершена.

--- Контрольная/Kontr_Klapatun.pl
+++ Контрольная/Kontr_Klapatun.pl
@@ -21,22 +21,11 @@ goal:- repeat,
 
 %...............................................................................
 
-proccess(5).
-
-%...............................................................................
-
 proccess(1):- write("Введите имя базы данных: "),
                       %read(NameBD),
                       consult("BD_Toys.txt"),
                       findall((Name,Cost), toy(Name,Cost), ToysList),
                       write_bd(ToysList).
-
-write_bd([]):- !.
-write_bd([H|T]):- writeln(H),
-                 write_bd(T),!.
-
-%...............................................................................
-%Add
 
 proccess(2):- nl,writeln("Добавление элемента:"),
               repeat,
@@ -49,13 +38,6 @@ proccess(2):- nl,writeln("Добавление элемента:"),
               read(Answ),nl,
               check_answer(Answ),!.
 
-check_answer(y):- fail.
-check_answer(n):- write("Готово"),nl.
-
-
-%...............................................................................
-%Delete
-
 proccess(3):- nl,writeln("Удаление записей."),nl,
               repeat,
               writeln("Введите название игрушки: "),
@@ -67,6 +49,33 @@ proccess(3):- nl,writeln("Удаление записей."),nl,
               read(Answ),nl,
               check_answer(Answ),!.
 
+proccess(4):- nl,writeln("Наиболее дорогие игрушки:"),nl,
+              most_expensive_toy.
+
+proccess(5):- findall((Name,Cost), toy(Name,Cost), ToysList),
+              open("BD_Toys.txt", write, F),
+              set_output(F),
+              write_in_file(ToysList),
+              close(F).
+
+%...............................................................................
+%1
+write_bd([]):- !.
+write_bd([H|T]):- writeln(H),
+                 write_bd(T),!.
+
+%...............................................................................
+%2
+%Add
+
+check_answer(y):- fail.
+check_answer(n):- write("Готово"),nl.
+
+
+%...............................................................................
+%3
+%Delete
+
 remove_record(Name):- findall((_,C),toy(Name,C),ListT),
                       write_elem(ListT).
 
@@ -76,29 +85,31 @@ write_elem([(N,C)|T]):- retract(toy(N,C)),
                         write_elem(T),!.
 
 %...............................................................................
+%4
 %Наиболее дорогие игрушки
 
-proccess(4):- nl,writeln("Наиболее дорогие игрушки:"),nl,
-              most_expensive_toy(ToysList),
-              nl,writeln("Хотите удалить еще что-то? (y - да; n - нет): "),
-              read(Answ),nl,
-              check_answer(Answ),!.
-
-most_expensive_toy(ToysList):- findall(C, toy(_,C), ToysListN),
+most_expensive_toy:- findall(C, toy(_,C), ToysListN),
                                msort(ToysListN, ToysListN2),
                                reverse(ToysListN2,ToysListN3),
                                nth0(0,ToysListN3,Max),
                                condition(ToysListN3, Max, _, _, ToysList),
-                               write_bd(ToysList).
-                                          
-                                          
+                               write_bd(ToysList),nl.
+
+
 condition([], ToysList, _, ToysList):- !.
 condition([H|T], Max, ToysListBefore, ToysListAfter, ToysList):- (Max-100)=< H,
-                                  findall((Name,H), toy(Name,H), TmpList),
-                                  append(ToysListBefore, TmpList, ToysListAfter),
-                                  condition(T, Max, ToysListAfter, _, ToysList).
-condition([_|T], _,ToysList,_,ToysList):- !.
-
-                                        
+                                                                 findall((Name,H), toy(Name,H), TmpList),
+                                                                 append(ToysListBefore, TmpList, ToysListAfter),
+                                                                 condition(T, Max, ToysListAfter, _, ToysList),!.
+condition([_|_], _,ToysList,_,ToysList):- !.
 
 
+
+%...............................................................................
+%5
+
+write_in_file([]):- !.
+write_in_file([(Name,Cost)|T]):- write('toy("'),write(Name),
+                                 write('",'),write(Cost),
+                                 write(').'),nl,
+                                 write_in_file(T),!.
